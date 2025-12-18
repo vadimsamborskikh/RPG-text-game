b42d9659a2616d2d213f5df4a0679c4a91089d07
vadimsamborskikh <vsamborskikh@yandex.ru>
2025-12-18 16:30:52 +0300

    refactor

--- game-logic.js
+++ game-logic.js
@@ -3,8 +3,9 @@ const bogsButton = document.getElementById("peat-bogs");
 const minesButton = document.getElementById("abandoned-mines");
 const villageButton = document.getElementById("village");
 
-forestButton.addEventListener("ckick", function() {
-    goToLocation("Мрачный лес");
+
+forestButton.addEventListener("ckick", () => {
+  goToLocation("Мрачный лес");
 });
 
 function goToLocation(locationName) {
@@ -12,19 +13,20 @@ function goToLocation(locationName) {
   gameState.gamePhase = "combat";
   //   gameState.currentEnemy = createRandomEnemy(); // TODO: написать функцию
 
-  updateUI(); //TODO: написать функцию
+  if (window.updateUI) {
+    updateUI();
+  }
 }
 
-function returnToVillage() {
-  gameState.currentLocation = "Деревня";
-  gameState.gamePhase = "exploration";
-  gameState.currentEnemy = null;
+// function returnToVillage() {
+//   gameState.currentLocation = "Деревня";
+//   gameState.gamePhase = "exploration";
+//   gameState.currentEnemy = null;
 
-  updateUI();
-}
+//   updateUI();
+// }
 
-function playerAttack() {
-  gameState.currentEnemy.hp -= gameState.hero.attack - gameState.currentEnemy.defense;
-
-  
-}
+// function playerAttack() {
+//   gameState.currentEnemy.hp -=
+//     gameState.hero.attack - gameState.currentEnemy.defense;
+// }
--- ui-controller.js
+++ ui-controller.js
@@ -1,3 +1,14 @@
 function updateUI() {
-    console.log('UI изменен');
+  const locationCard = document.querySelector("location-card");
+  locationCard.innerHTML = `
+                        <h2>Лес</h2>
+                        <p id="location-description">Вы стоите на центральной площади небольшой деревушки. Деревянные дома с
+                            соломенными крышами выстроились вокруг мощёной камнем площади. В воздухе пахнет дымом из печей и
+                            свежескошенной травой.
+                        </p>
+                        `;
+console.log('HI from updateUI');
+
 }
+
+window.updateUI = updateUI;
